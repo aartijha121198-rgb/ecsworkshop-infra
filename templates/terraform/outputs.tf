ec2e72a77814412c341bc5f6221945183911c9f1
Aaru Jha <aarujha123@gmail.com>
2026-05-10 21:59:12 +0530

    s3 bucket point done

--- templates/terraform/outputs.tf
+++ templates/terraform/outputs.tf
@@ -1,7 +1,19 @@
+output "vpc_id" {
+  value = module.network.vpc_id
+}
+
 output "alb_dns_name" {
   value = module.ecs.alb_dns_name
 }
 
 output "ecr_repo_url" {
   value = module.ecs.ecr_repo_url
+}
+
+#output "ecs_cluster_name" {
+ # value = module.ecs.cluster_name
+#}
+
+output "terraform_state_bucket" {
+  value = "jenkins-tf-state-bucket-aaru-labs"
 }
